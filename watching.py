e0af731802135d657ac6eaa2483b09b632ce0e0e
sujang Jo <46040847+josoooooooooojang@users.noreply.github.com>
2021-03-10 14:36:55 +0900

    [Update] 알림 처리 완료

--- watching.py
+++ watching.py
@@ -18,41 +18,34 @@ twitter_api = twitter.Api(consumer_key=consumer_key,
 # 최신 트윗 유지 파일 생성
 latest_text_file_path = './latest.txt'
 latest_tweet = ''
-if not (os.path.isfile(latest_text_file_path)):
-    f = open(latest_text_file_path, 'w', encoding = 'utf-8')
-    f.write("test")
-    f.close
+if os.path.isfile(latest_text_file_path):
+    with open(latest_text_file_path, 'r', encoding = 'utf-8') as f:
+        latest_tweet = f.readline()
+
+if latest_tweet == '':
     statuses = twitter_api.GetUserTimeline(screen_name=account, count=5, include_rts=False, exclude_replies=False)
     latest_tweet = statuses[0].text
-else:
-    f = open(latest_text_file_path, 'r')
-    latest_tweet = f.readline()
-    f.close
 
 print("[Notice] The latest tweet is " + latest_tweet)
     
 # 트윗 감지 시작
 while(True):
     # 트위터 내 최신 트윗 가져오기
+    time.sleep(10)
     statuses = twitter_api.GetUserTimeline(screen_name=account, count=5, include_rts=False, exclude_replies=False)
     new_tweet = statuses[0].text
 
+    # 트윗 업데이트가 있을 경우
     if latest_tweet != new_tweet:
-        # 트윗 업데이트가 있을 경우
         print("[Notice] The tweet is diffrent.")
 
-        # 파일의 내용을 최신으로 변경
-        f = open(latest_text_file_path, 'w')
-        f.write(new_tweet)
-        f.close
-        print("[Notice] \"" + latest_tweet + "\" to \"" + new_tweet + "\"")
+        with open(latest_text_file_path, 'w') as f:
+            f.write(new_tweet)
+        
         latest_tweet = new_tweet
+        print("[Notice] \"" + latest_tweet + "\" to \"" + new_tweet + "\"")
     else:
         print("[Notice] Nothing is changed.")
-
-    time.sleep(10)
-
-    
 
 
     
